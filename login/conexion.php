318a24eee33b1610b15d21eb80df0e3c4d2f23de
dawnunderban <dawnunderban@gmail.com>
2025-10-17 09:35:39 -0500

    av

--- login/conexion.php
+++
@@ -1,7 +0,0 @@
-<?php
-$conexion = new mysqli("localhost", "root", "", "login_registro_gym");
-
-if ($conexion->connect_error) {
-    die("Error de conexión: " . $conexion->connect_error);
-}
-?>
--- login/mensajes_login.php
+++
@@ -1,7 +0,0 @@
-<?php
-if (isset($_GET['error'])) {
-    echo "<div class='error-box'>
-            <p>⚠️ Usuario o contraseña incorrectos</p>
-          </div>";
-}
-?>
